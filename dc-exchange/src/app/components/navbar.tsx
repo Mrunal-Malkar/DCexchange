471f8cba8d6ee3b47555a8168734788307c3b778
Mrunal-Malkar <mrunalpmalkar@gmail.com>
2025-09-18 00:04:34 +0530

    added fix issue

--- dc-exchange/src/app/components/navbar.tsx
+++ dc-exchange/src/app/components/navbar.tsx
@@ -4,7 +4,7 @@ export const Navbar=()=>{
         <div className="text-2xl">DC-<span className="text-green-500">exchange</span></div>
         <div className="text-xl">
             <button className="p-2 flex justifiy-auto ">
-                <div className=" bg-gray-600 p-1 border-e-2 border-amber-600">Log-in</div>  
+                <div className=" bg-gray-600 p-1 border-e-2 border-amber-600">Log-in</div>  {/* fix: border not visible*/}
                 <div className=" bg-blue-500 p-1">Google</div>
             </button>
         </div>

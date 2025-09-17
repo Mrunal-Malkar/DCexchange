eedf01278d1fae6dc78dd3578dabbaac4350e616
Mrunal-Malkar <mrunalpmalkar@gmail.com>
2025-09-18 00:03:27 +0530

    added navbar

---
+++ dc-exchange/src/app/components/navbar.tsx
@@ -0,0 +1,12 @@
+export const Navbar=()=>{
+    return(
+    <div className="flex p-4 text-gray-500 justify-between min-w-full">
+        <div className="text-2xl">DC-<span className="text-green-500">exchange</span></div>
+        <div className="text-xl">
+            <button className="p-2 flex justifiy-auto ">
+                <div className=" bg-gray-600 p-1 border-e-2 border-amber-600">Log-in</div>  
+                <div className=" bg-blue-500 p-1">Google</div>
+            </button>
+        </div>
+    </div>)
+}

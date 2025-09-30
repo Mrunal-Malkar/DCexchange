06cf32c65370b14a5f27f8cd449cead1a5498f5c
Mrunal-Malkar <mrunalpmalkar@gmail.com>
2025-10-01 01:38:23 +0530

    feat: update image handling in Client component and enhance Loader SVG attributes

--- dc-exchange/src/app/dashboard/client.tsx
+++ dc-exchange/src/app/dashboard/client.tsx
@@ -1,20 +1,40 @@
+"use client";
 import { Wallet } from "lucide-react";
+import { useSession } from "next-auth/react";
+import Image from "next/image";
 import toast, { Toaster } from "react-hot-toast";
 
 const Client = ({ welcome }: { welcome?: boolean }) => {
   if (welcome === true) {
     toast.success("Welcome to DC Exchange, you are now signed-in!");
   }
+  const {data}=useSession()
+  const nameArray=(data?.user?.name)?.split(" ");
+  console.log(nameArray);
+  const name=nameArray?.map((i)=>{
+    const firstLetter=i[0].toUpperCase();
+    const restOfName=i.slice(1);
+    if(i==nameArray[0]) return firstLetter+restOfName+" ";
+    return firstLetter+restOfName;
+  })
 
   return (
     <div className="min-h-[90vh] min-w-screen w-screen flex md:p-0 p-4 items-start justify-center">
       <Toaster />
       <div className="h-[600px] w-full md:w-[803px] grid grid-rows-2 md:mt-18 mt-4 rounded-2xl shadow-2xl">
         <div className="px-8 py-4 bg-white grid grid-rows-3 min-h-1/2 min-w-full rounded-t-2xl border-transparent">
-          <div className="w-full flex justify-start items-center">
-            <div className="w-18 h-18 bg-black">Profile image</div>
+          <div className="w-full flex justify-start gap-x-0.5 items-center">
+            <div className="w-18 h-18 ">
+              <Image 
+              className="w-full h-full rounded-4xl"
+              src={data?.user?.image as string}
+              width={18}
+              height={18}
+              alt="profile-picture"
+              />
+            </div>
             <div className="flex text-2xl font-semibold justify-center items-center">
-              <h1>Your Name</h1>
+              <h1>{name}</h1>
             </div>
           </div>
           <div className="w-full flex flex-col justify-center items-center gap-y-0">
